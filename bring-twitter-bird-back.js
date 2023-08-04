6d143dd76b71f98c907ee66777c90e6176a7859a
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2023-08-04 13:58:37 +0200

    Updated README.md and improved formatting of manifest.json

--- bring-twitter-bird-back.js
+++ bring-twitter-bird-back.js
@@ -7,6 +7,64 @@ If a copy of the MPL was not distributed with this file, You can obtain one at h
 
 console.log("Bring Twitter Bird Back extension has loaded.");
 
+
+// When one of the selectors is found, replaces the svg, then disconnects the observer.
+const bodyCallback = (mutationList, observer) => {
+	console.log("greetings");
+	for (let mutation of mutationList) {
+		console.log("greetings 2");
+		if (document.querySelector(querySelectorInput)) {
+			logoFound = true;
+			var logoSvg = document.querySelector(querySelectorInput).parentNode.parentNode;
+			logoSvg.getElementsByTagName("path")[0].setAttribute("d", "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z");
+			logoSvg.setAttribute("viewBox", "0 0 24 24");
+
+			// Checks if the element contains the class used for the default background theme/color (white),
+			// in which case it sets the SVG's color to blue.
+			if (theme == "light") {
+				logoSvg.setAttribute("style", "color: #1D9BF0;");
+			}
+			if (notificationFound && postButtonsFound) {
+				observer.disconnect();
+			}
+		} else if (document.querySelector('a[href="/notifications"][role="link"]') && !notificationFound) {
+			notificationFound = true;
+			startNotificationObserver();
+			if (logoFound && postButtonsFound) {
+				observer.disconnect();
+			}
+		} else if (document.querySelector(navTweetButtonSelector) && document.querySelector(inlineTweetButtonSelector) && !postButtonsFound) {
+			postButtonsFound = true;
+			startPostButtonsObserver();
+			if (logoFound && notificationFound) {
+				observer.disconnect();
+			}
+		}
+	}
+}
+const bodyObserver = new MutationObserver(bodyCallback);
+
+
+const notificationCallback = (mutationList, observer) => {
+	console.log("yo 2");
+	for (let mutation of mutationList) {
+		console.log("yo 3");
+		updateFavicon();
+	}
+}
+const notificationObserver = new MutationObserver(notificationCallback);
+
+
+const postButtonsCallback = (mutationList, observer) => {
+	for (let mutation of mutationList) {
+		console.log("hello there 3");
+		navTweetButton.textContent = "Tweet";
+		inlineTweetButton.textContent = "Tweet";
+	}
+}
+const postButtonsObserver = new MutationObserver(postButtonsCallback);
+
+
 function updateFavicon(faviconPath = "icons/favicon.ico") {
 	// Iterates through <link> elements to find the icon, then removes it.
 	const elements = document.getElementsByTagName("link");
@@ -47,45 +105,45 @@ switch (document.body.style.backgroundColor) {
 
 // Defines selectors associated with the <svg> element for the logo.
 const querySelectorInput = 'path[d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"]'
+const navTweetButtonSelector = 'a[data-testid="SideNav_NewTweet_Button"]';
+const inlineTweetButtonSelector = 'div[data-testid="tweetButtonInline"]'
 
 var logoFound = false;
 var notificationFound = false;
-// When one of the selectors is found, replaces the svg, then disconnects the observer.
-const logoCallback = (mutationList, observer) => {
-	for (let mutation of mutationList) {
-		if (document.querySelector(querySelectorInput)) {
-			logoFound = true;
-			var logoSvg = document.querySelector(querySelectorInput).parentNode.parentNode;
-			logoSvg.getElementsByTagName("path")[0].setAttribute("d", "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z");
-			logoSvg.setAttribute("viewBox", "0 0 24 24");
+var postButtonsFound = false;
 
-			// Checks if the element contains the class used for the default background theme/color (white),
-			// in which case it sets the SVG's color to blue.
-			if (theme == "light") {
-				logoSvg.setAttribute("style", "color: #1D9BF0;");
-			}
-			if (notificationFound) {
-				observer.disconnect();
-			}
-		} else if (document.querySelector('a[href="/notifications"][role="link"]') && !notificationFound) {
-			notificationFound = true;
-			startNotificationObserver();
-			if (logoFound) {
-				observer.disconnect();
-			}
-		}
-	}
-}
 // Creates and initiates an observer.
-const logoObserver = new MutationObserver(logoCallback);
-logoObserver.observe(document.body, { childList: true, subtree: true });
+bodyObserver.observe(document.body, { childList: true, subtree: true });
 
 function startNotificationObserver() {
-	const notificationCallback = (mutationList, observer) => {
+	console.log("yo");
+	notificationObserver.observe(document.querySelector('a[href="/notifications"][role="link"]'), { childList: true, subtree: true });
+}
+
+function startMainObserver() {
+	let main = document.querySelector("main");
+	const mainObserverCallback = (mutationList, observer) => {
 		for (let mutation of mutationList) {
-			updateFavicon();
+			if (main.querySelector('div[data-testid="tweetButtonInline"]')) {
+				startPostButtonsObserver();
+			} else {
+				postButtonsObserver.disconnect();
+			}
 		}
 	}
-	const notificationObserver = new MutationObserver(notificationCallback);
-	notificationObserver.observe(document.querySelector('a[href="/notifications"][role="link"]'), { childList: true, subtree: true });
+
+	const mainObserver = new MutationObserver(mainObserverCallback);
+	mainObserver.observe(main);
+}
+
+function startPostButtonsObserver() {
+	console.log("hello there");
+	let navTweetButton = document.querySelector(navTweetButtonSelector).getElementsByTagName("span")[2];
+	let inlineTweetButton = document.querySelector(inlineTweetButtonSelector).getElementsByTagName("span")[1];
+	console.log(navTweetButton);
+	console.log(inlineTweetButton);
+	console.log("hello there 2");
+	
+	postButtonsObserver.observe(document.querySelector('a[data-testid="SideNav_NewTweet_Button"]'), { characterData: true });
+	postButtonsObserver.observe(document.querySelector('div[data-testid="tweetButtonInline"]'), { characterData: true });
 }
