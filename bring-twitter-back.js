b3f206e3b0f8875047d8a5ae98ebbd839a35eeb5
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2024-07-25 17:04:26 +0200

    Reverted change that caused a bug to replace X in random places

--- bring-twitter-back.js
+++ bring-twitter-back.js
@@ -8,6 +8,7 @@ If a copy of the MPL was not distributed with this file, You can obtain one at h
 
 console.log("Bring Twitter Bird Back extension has loaded.");
 
+const twitterLogoD = "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z";
 
 // Defines selectors for elements.
 const querySelectorInput = 'path[d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"]';
@@ -22,6 +23,7 @@ const profileTweetsTextSelector = 'a[role="tab"]';
 const tweetPostTitleSelector = 'h2[dir="ltr"][aria-level="2"][role="heading"]';
 const loginFooterSelector = 'span[class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3"]';
 const cookieBannerSelector = 'div[class="css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-1qd0xha r-n6v787 r-1cwl3u0 r-16dba41 r-5oul0u r-knv0ih"]';
+const loadingLogoSelector = 'svg[class="r-4qtqp9 r-yyyyoo r-dnmrzs r-lrvibr r-m6rgpd r-1p0dtai r-1nao33i r-wy61xf r-zchlnj r-1d2f490 r-ywje51 r-u8s1d r-ipm5af r-1blnp2b"] path';
 
 var notificationObserverConnected = false;
 
@@ -82,7 +84,7 @@ const bodyCallback = (mutationList, observer) => {
 		const querySelectorResult = document.querySelector(querySelectorInput);
 		if (querySelectorResult) {
 			var logoSvg = querySelectorResult.parentNode.parentNode;
-			logoSvg.getElementsByTagName("path")[0].setAttribute("d", "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z");
+			logoSvg.getElementsByTagName("path")[0].setAttribute("d", twitterLogoD);
 			logoSvg.setAttribute("viewBox", "0 0 24 24");
 
 			// Checks if the element contains the class used for the default background theme/color (white),
@@ -155,6 +157,7 @@ const bodyCallback = (mutationList, observer) => {
 			}
 		}
 
+		/*
 		const loginFooterResult = document.querySelectorAll(loginFooterSelector);
 		if (loginFooterResult) {
 			for (result of loginFooterResult) {
@@ -163,7 +166,7 @@ const bodyCallback = (mutationList, observer) => {
 				}
 			}
 		}
-
+		*/
 		const cookieBannerResult = document.querySelector(cookieBannerSelector);
 		if (cookieBannerResult && cookieBannerResult.textContent.includes("X")) {
 			cookieBannerResult.textContent = cookieBannerResult.textContent.replace("X", "Twitter");
@@ -215,3 +218,20 @@ const titleObserver = new MutationObserver(titleCallback);
 function startTitleObserver() {
 	titleObserver.observe(document.querySelector("title"), { childList: true });
 }
+
+const loadingLogoObserverCallback = (mutationList, observer) => {
+	for (let mutation of mutationList) {
+		console.log("CHANGE");
+		updateLogo();
+		loadingLogoObserver.disconnect();
+	}
+}
+const loadingLogoObserver = new MutationObserver(loadingLogoObserverCallback);
+loadingLogoObserver.observe(document.querySelector(loadingLogoSelector), { childList: true, subtree: true });
+
+function updateLogo() {
+	const loadingLogo = document.querySelector(loadingLogoSelector);
+	loadingLogo.setAttribute("d", twitterLogoD);
+}
+
+updateLogo();
