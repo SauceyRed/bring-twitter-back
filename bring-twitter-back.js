47c8ed9678ac0cb11ed178e4dd9e1c6be2348cff
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2024-07-28 18:37:14 +0200

    Updated copyright year

--- bring-twitter-back.js
+++ bring-twitter-back.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 SauceyRed
+Copyright 2023-2024 SauceyRed
 
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
