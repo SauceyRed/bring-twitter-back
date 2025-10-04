50fe9733125f2976524879717a0758b1b9c1124d
SauceyRed <saucey@saucey.red>
2025-10-05 01:41:40 +0200

    feat: rename querySelectorInput to xLogoSelector

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ console.log("Bring Twitter Back extension has loaded.");
 const twitterLogoD = "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z";
 
 // Defines selectors for elements.
-const querySelectorInput = 'path[d="M21.742 21.75l-7.563-11.179 7.056-8.321h-2.456l-5.691 6.714-4.54-6.714H2.359l7.29 10.776L2.25 21.75h2.456l6.035-7.118 4.818 7.118h6.191-.008zM7.739 3.818L18.81 20.182h-2.447L5.29 3.818h2.447z"]';
+const xLogoSelector = 'path[d="M21.742 21.75l-7.563-11.179 7.056-8.321h-2.456l-5.691 6.714-4.54-6.714H2.359l7.29 10.776L2.25 21.75h2.456l6.035-7.118 4.818 7.118h6.191-.008zM7.739 3.818L18.81 20.182h-2.447L5.29 3.818h2.447z"]';
 const notificationsSelector = 'a[href="/notifications"][role="link"]';
 const tweetButtonsSelector = 'a[href="/compose/post"]';
 const homeTweetButtonSelector = '[data-testid="tweetButtonInline"]';
@@ -119,7 +119,7 @@ updateLogo();
 // When one of the selectors is found, replaces the svg, then disconnects the observer.
 const bodyCallback = (mutationList: MutationRecord[], observer: MutationObserver) => {
 	for (let _ of mutationList) {
-		const querySelectorResult = document.querySelector(querySelectorInput);
+		const querySelectorResult = document.querySelector(xLogoSelector);
 		if (querySelectorResult && querySelectorResult.parentElement && querySelectorResult.parentElement.parentElement) {
 			const logoSvg = querySelectorResult.parentElement.parentElement;
 			logoSvg.getElementsByTagName("path")[0].setAttribute("d", twitterLogoD);
